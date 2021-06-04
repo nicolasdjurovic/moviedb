28ebb9f0b340e47934a9f18d23e32dd81e68e4a0
Nicolas Djurovic <nicolasdjurovic@outlook.com>
2021-06-04 11:38:30 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,110 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="inputcontainer">
+      <input type="text" placeholder="Search a movie or show..." v-model="searchTerm"/>
+      <select v-model="searchType">
+        <option value="movie" selected>Movies</option>
+        <option value="series">Series</option>
+        <option value="episode">Episodes</option>
+      </select>
+      <button @click="searchDB">Search</button>
+    </div>
+    <div class="movielistcontainer">
+      <movieList v-bind:movieDetails="this.searchResults" />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import movieList from "./components/movieList";
+import axios from "axios";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      searchTerm: "",
+      searchType: "",
+      searchResults: {},
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    movieList,
+  },
+  methods: {
+    searchDB() {
+      axios
+        .get("http://www.omdbapi.com/", {
+          params: {
+            apikey: "2b2c454e",
+            s: this.searchTerm,
+            type: this.searchType,
+          },
+        })
+        .then((response) => {
+          this.searchResults = response.data.Search;
+        });
+    },
+  },
+};
 </script>
 
 <style lang="scss">
+
+*{
+  padding: 0;
+  border: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.container {
+  width: 100%;
+  height: 400px;
+}
+
+.inputcontainer {
+  height: 50px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  input {
+    width: 100%;
+    height: 50px;
+    background-color: #2c3e50;
+    color: white;
+    padding-left: 20px;
+  }
+
+  input::placeholder{
+    color: white;
+    padding: 20px;
+  }
+
+  button {
+    height: 50px;
+    width: 100px;
+    cursor: pointer;
+    background-color: rgb(255, 208, 0);
+  }
+
+  select{
+    height: 50px;
+    width: 100px;
+        background-color: rgb(255, 208, 0);
+  }
+
+  select:focus, input:focus{
+    outline: none;
+  }
+
+
 }
 </style>
---
+++ src/components/movieList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div
+    class="movielist"
+    v-for="(item, index) in movieDetails"
+    v-bind:key="index"
+  >
+    <img class="poster" v-bind:src="item.Poster" alt="" />
+    <div class="right">
+      <div class="title">{{ item.Title }}</div>
+      <div class="year">{{ item.Year }}</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "movieList",
+  props: {
+    movieDetails: Array,
+  },
+};
+</script>
+
+<style>
+.movielist {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  height: 100px;
+  background-color: white;
+  border-bottom: 1px solid black;
+}
+
+.right{
+    display: flex;
+    justify-content: flex-start;
+    margin-left: 10px;
+}
+
+.poster {
+  height: 80px;
+}
+
+.title{
+    font-weight: 700;
+}
+
+.year{
+    font-style: italic;
+    margin-left: 10px;
+}
+</style>
